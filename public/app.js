44828f4f4dbd73cc61a3581267df7acd88290baa
DarwinDiaz07 <darwindiazher@gmail.com>
2026-03-18 23:17:36 -0500

    color palete updated

--- public/app.js
+++ public/app.js
@@ -30,11 +30,9 @@ let marcador = null;
 let polilinea = null;
 const coordenadas = [];
 
-// CartoDB Dark Matter tiles
-L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>',
-  subdomains: "abcd",
+// OpenStreetMap tiles
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   maxZoom: 19,
 }).addTo(mapa);
 
